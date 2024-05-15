9885d588811ab37021b34536d016dbce7b6fe801
tas_admin <tas_admin@hq.int.unesco.org>
2024-05-15 19:53:23 +0500

    add mbPartsVM & upd Mb/Create

---
+++ RepairBase/Models/VM/MbPartsVM.cs
@@ -0,0 +1,9 @@
+﻿namespace RepairBase.Models.VM
+{
+    public class MbPartsVM
+    {
+        public Mb mb { get; set; } = new Mb();
+        public string[] strVideoSrcLocated { get; set; } = [];
+        public string[] strPartsPNs { get; set; } = [];
+    }
+}
