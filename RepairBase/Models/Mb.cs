f85cf30b916352ce0f65db51a25ddb5f2d0ab353
tas_admin <tas_admin@hq.int.unesco.org>
2024-05-18 17:07:58 +0500

    upd MbRepair/Insex

--- RepairBase/Models/Mb.cs
+++ RepairBase/Models/Mb.cs
@@ -28,22 +28,39 @@ namespace RepairBase.Models
         [Required]
         public string PN { get; set; }     // !3-PN->UPPERCASE!
         public string? CompatiblePN { get; set; }// multiple rec
+        [Required]
         public string Parts4test { get; set; }  // y/n/other
+        [Required]
         public string How2start { get; set; }
+        [Required]
         public string RAMtype { get; set; }    //DDR2-5, builtin
+        [Required]
         public int RAMspeed { get; set; }       //Mhz
+        [Required]
         public string RAMslot { get; set; }
+        [Required]
         public Boolean BuiltinSSD { get; set; }//y/n
+        [Required]
         public Boolean Post { get; set; }      //y/n
+        [Required]
         public string Video { get; set; }      //n/wtLCD/extScr
+        [Required]
         public string VideoInfo { get; set; }  //Post/OS/Blank
+        [Required]
         public string VideoSrcLocated { get; set; }  //HDMI/rightType-C/..
-        public OStype  OS { get; set; }        
+        [Required]
+        public OStype  OS { get; set; }
+        [Required]
         public string AdapterBrand { get; set; }
+        [Required]
         public string AdapterWattage { get; set; }
+        [Required]
         public string AdapterPN { get; set; }
+        [Required]
         public string PowerAdapterType { get; set; } //ACjack/direct2board
+        [Required]
         public string RepairInfo { get; set; }       //comment
+        [Required]
         public DateTime DateAdded { get; set; }
         public DateTime UserAdded { get; set; }
         //  public int PartsID                  //----Mb2Parts---
